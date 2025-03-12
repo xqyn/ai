fd089da8d4fd97d3e590fea8e2b89c30944af66a
xqyn <nxquy.bms@gmail.com>
2025-03-12 13:12:30 +0100

    cs50ai

---
+++ cs50ai/lecture5-neural/banknotes/test.py
@@ -0,0 +1,17 @@
+import csv
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+# Read data in from file
+with open("banknotes.csv") as f:
+    reader = csv.reader(f)
+    next(reader)
+
+    data = []
+    for row in reader:
+        data.append({
+            "evidence": [float(cell) for cell in row[:4]],
+            "label": 1 if row[4] == "0" else 0
+        })
+#python
